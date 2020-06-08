fb03cab33191bcca92817d31958365215b978cf5
siarhei-k <s.k@live.ru>
2020-06-08 17:37:25 +0300

    Stepik (re.search)

---
+++ findall2.py
@@ -0,0 +1,12 @@
+"""
+Вам дана последовательность строк.
+Выведите строки, содержащие "cat" в качестве слова.
+"""
+
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(r'\bcat\b', line):
+        print('\n' + line, end='')
