864e860381e590fc3b730eb6521790b6d914f087
siarhei-k <s.k@live.ru>
2020-06-08 17:58:22 +0300

    Stepik

---
+++ findall4.py
@@ -0,0 +1,12 @@
+"""
+Вам дана последовательность строк.
+Выведите строки, содержащие обратный слеш "\".
+"""
+
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(r'\\', line):
+        print('\n' + line, end='')
