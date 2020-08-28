e74cd1bf40e2e3802cc1c73cf6ef1dd81d17fb7c
siarhei_karelau <siarhei.karelau@gmail.com>
2020-08-28 17:21:09 +0300

    DataQuest (pandas)

---
+++ top_employer.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+f500 = pd.read_csv('f500.csv')
+
+f500.loc[f500["previous_rank"] == 0, "previous_rank"] = np.nan
+
+top_employer_by_country = {}
+
+countries = f500['country'].unique()
+
+for c in countries:
+    top_employer_by_country[c] = f500[f500['country'] == c].sort_values("employees", ascending=False).iloc[0].loc['company']
+
+print(top_employer_by_country)
+
